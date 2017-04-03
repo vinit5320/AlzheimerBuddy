b9d8169936f0f065d1c30fd1847e7f2e6891a3f7
vinit5320 <vinit5320@gmail.com>
2017-04-03 17:10:46 -0400

    setup Amazon Alexa on xcode

---
+++ iOS/AlzBud/AlzBud-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  AlzBud-Bridging-Header.h
+//  AlzBud
+//
+//  Created by Vinit Jasoliya on 4/3/17.
+//  Copyright © 2017 Vinit Jasoliya. All rights reserved.
+//
+
+#ifndef AlzBud_Bridging_Header_h
+#define AlzBud_Bridging_Header_h
+
+#import <LoginWithAmazon/LoginWithAmazon.h>
+
+#endif /* AlzBud_Bridging_Header_h */
