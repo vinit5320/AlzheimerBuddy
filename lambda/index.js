966f07ff53b36bb47b1649b0af7e1c81e204b553
vinit5320 <vinit5320@gmail.com>
2017-05-06 00:47:08 -0400

    Add DB Connection, complete things, complete memories

--- lambda/index.js
+++ lambda/index.js
@@ -1,192 +1,214 @@
-/**
+var https = require('https')
+var mysql = require('mysql')
 
- Copyright 2016 Brian Donohue.
+var connection;
 
-*/
+exports.handler = (event, context) => {
 
-'use strict';
-
-// Route the incoming request based on type (LaunchRequest, IntentRequest,
-// etc.) The JSON body of the request is provided in the event parameter.
-exports.handler = function (event, context) {
     try {
-        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
 
-        /**
-         * Uncomment this if statement and populate with your skill's application ID to
-         * prevent someone else from configuring a skill that sends requests to this function.
-         */
-
-//     if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.05aecccb3-1461-48fb-a008-822ddrt6b516") {
-//         context.fail("Invalid Application ID");
-//      }
+        connection = mysql.createConnection({
+            host     : 'mysqlforlambdatest.cxjr41nyvqjk.us-east-1.rds.amazonaws.com',
+            user     : 'keval',
+            ssl : 'Amazon RDS',
+            password : 'mypassword',
+            database : 'ExampleDB',
+            port : '3306',
+        });
 
         if (event.session.new) {
-            onSessionStarted({requestId: event.request.requestId}, event.session);
+            newSessionHelper(event, context);
+        } else {
+            oldSessionHelper(event, context);
         }
 
-        if (event.request.type === "LaunchRequest") {
-            onLaunch(event.request,
-                event.session,
-                function callback(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === "IntentRequest") {
-            onIntent(event.request,
-                event.session,
-                function callback(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === "SessionEndedRequest") {
-            onSessionEnded(event.request, event.session);
-            context.succeed();
-        }
-    } catch (e) {
-        context.fail("Exception: " + e);
-    }
-};
 
-/**
- * Called when the session starts.
- */
-function onSessionStarted(sessionStartedRequest, session) {
-    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
-        + ", sessionId=" + session.sessionId);
-
-    // add any session init logic here
-}
-
-/**
- * Called when the user invokes the skill without specifying what they want.
- */
-function onLaunch(launchRequest, session, callback) {
-    console.log("onLaunch requestId=" + launchRequest.requestId
-        + ", sessionId=" + session.sessionId);
-
-    var cardTitle = "Hello, World!"
-    var speechOutput = "You can tell Hello, World! to say Hello, World!"
-    callback(session.attributes,
-        buildSpeechletResponse(cardTitle, speechOutput, "", true));
-}
-
-/**
- * Called when the user specifies an intent for this skill.
- */
-function onIntent(intentRequest, session, callback) {
-    console.log("onIntent requestId=" + intentRequest.requestId
-        + ", sessionId=" + session.sessionId);
-
-    var intent = intentRequest.intent,
-        intentName = intentRequest.intent.name;
-
-    // dispatch custom intents to handlers here
-    if (intentName == 'TestIntent') {
-        handleTestRequest(intent, session, callback);
-    } else if (intentName == 'AMAZON.findThings') {
-        handleThingsRequest(intent, session, callback);
-    } else if (intentName == 'FindingObjectContext') {
-        handleFindingThingsRequest(intent, session, callback);
-    }
-    else {
-        throw "Invalid intent";
-    }
-}
-
-/**
- * Called when the user ends the session.
- * Is not called when the skill returns shouldEndSession=true.
- */
-function onSessionEnded(sessionEndedRequest, session) {
-    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
-        + ", sessionId=" + session.sessionId);
-
-    // Add any cleanup logic here
-}
-
-function handleTestRequest(intent, session, callback) {
-    callback(session.attributes,
-        buildSpeechletResponseWithoutCard("Hello, World Vinit!", "", "true"));
-}
-
-function handleThingsRequest(intent, session, callback) {
-
-    var slots = intent.slots;
-    var itemName = "";
-    for(var key in slots){
-        itemName = slots[key].value;
-    }
-    if(itemName && itemName !== ""){
-        callback(session.attributes,
-        buildSpeechletResponseWithoutCard("Found your "+itemName, "", "true"));
-    }else {
-        callback(session.attributes,
-        buildSpeechletResponseWithoutCard("Found your item", "", "true"));
-    }
-}
-
-function handleFindingThingsRequest(intent, session, callback) {
-
-    var slots = intent.slots;
-    var itemName = "";
-    for(var key in slots){
-        if(slots[key].hasOwnProperty('value')){
-            itemName = slots[key].value;
-        }
-    }
-
-    if(itemName === "") {
-        callback(session.attributes,
-        buildSpeechletResponseWithoutCard("What is it that you are looking for ?", "", "true"));
-    } else {
-        callback(session.attributes,
-        buildSpeechletResponseWithoutCard("Found your "+itemName, "", "true"));
-    }
+    } catch(error) { context.fail('Exception: '+error) }
 
 }
 
-// ------- Helper functions to build responses -------
+newSessionHelper = (event,context) => {
+    switch (event.request.type) {
 
-function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+        case "LaunchRequest":
+            // Launch Request
+            respondBack("You can ask for finding things, sharing memories, family information, etc.", true, {}, context);
+            break;
+
+        case "IntentRequest":
+            // Intent Request
+
+            switch(event.request.intent.name) {
+
+                case "FindThings":
+                    var itemName = event.request.intent.slots.thingsslot.value;
+                    var query = "SELECT location from things where name = '" + itemName + "'";
+                    getDBResponse(query, itemName, context, "select", ""+event.request.intent.name);
+                    break;
+
+                case "FindingObjectContext":
+                    respondBack('What is it that you are looking for ?', false, {
+                        "context": "FindingObjectContext"
+                    }, context);
+                    break;
+
+                case "SaveThingsContext":
+                    var itemName = event.request.intent.slots.thingsLocation.value;
+                    respondBack('Where are you exactly keeping your ' + itemName, false, {
+                        "itemName": itemName,
+                        "context": "SaveThingsContext"
+                    }, context);
+                    break;
+
+                case "OthersMemory":
+                    var userName = "vinit";
+                    if(event.request.intent.slots.personName.hasOwnProperty('value')){
+                        userName = ""+event.request.intent.slots.personName.value;
+                    }
+                    var query = "SELECT memory from memories where person = '" + userName + "'";
+                    getDBResponse(query, userName, context, "select", ""+event.request.intent.name);
+                    break;
+
+                default:
+                    respondBack("Sorry I cannot handle your request right now.", true,{},context);
+                    break;
+
+            }
+            break;
+
+        case "SessionEndedRequest":
+            // Session Ended Request
+            break;
+
+        default:
+            context.fail('INVALID REQUEST TYPE: '+event.request.type)
+
+    }
+}
+
+oldSessionHelper = (event,context) => {
+
+    var itemName,params,currentContext,query,prevContext;
+
+    prevContext = event.session.attributes.context;
+    currentContext = event.request.intent.name;
+
+    if(currentContext === "FindingObjectContext"){
+        itemName = event.request.intent.slots.thingsslot.value;
+    } else if(currentContext === "SaveThingsLocation") {
+        itemName = event.session.attributes.itemName;
+        params = event.request.intent.slots.thingsLocation.value;
+    } else if(currentContext === "DecisionBool") {
+        itemName = event.session.attributes.itemName;
+        params = event.request.intent.slots.yesNoBool.value;
+    }
+
+    switch(true) {
+
+        case currentContext === "FindingObjectContext":
+            query = "SELECT location FROM things WHERE name = '" + itemName + "'";
+            getDBResponse(query, itemName, context, "select",currentContext);
+            break;
+
+        case currentContext === "SaveThingsLocation" && prevContext === "SaveThingsContext":
+            params = params.replace(/my/g, 'your');
+            params = params.replace(/mine/g, 'yours');
+            query = "INSERT INTO things (name, location) VALUES ( '"+itemName+"', '"+params+"')";
+            getDBResponse(query, itemName, context,"insert", currentContext);
+            break;
+
+        case currentContext === "DecisionBool" && prevContext === "SaveThingsContext":
+            if(params === "yes") {
+                respondBack('Where are you exactly keeping your ' + itemName, false, {
+                    "itemName": itemName,
+                    "context": "SaveThingsContext"
+                }, context);
+            } else {
+                respondBack("Okay, what else can I help you with ?", true,{},context);
+            }
+            break;
+
+        default:
+            respondBack("Sorry I cannot handle your request right now.", true,{},context);
+            break;
+
+    }
+}
+
+//Reponse
+respondBack = (response, endSession, attributes, context) => {
+    context.succeed(
+        generateResponse(
+            buildSpeechletResponse(response, endSession),
+            attributes
+        )
+    )
+}
+
+//DB Connection
+getDBResponse = (query, itemName, context, type, intentContext) => {
+
+    if(type === "insert") {
+        connection.query(query, function(err, rows) {
+            if (err) throw err;
+            if(intentContext && intentContext === "SaveThingsLocation") {
+                respondBack("Okay I will remember that location permanently", true, {}, context);
+            } else {
+                respondBack("Sorry I cannot handle your request right now.", true, {}, context);
+            }
+        });
+
+
+    } else if(type === "select") {
+        connection.query(query, function (err, rows) {
+            if (err) throw err;
+
+            if(intentContext && (intentContext === "FindThings" || intentContext === "FindingObjectContext")) {
+                if (rows[0] !== undefined) {
+                    var loc = rows[0].location;
+                    respondBack(loc, true, {}, context);
+                } else {
+                    respondBack("Sorry I don't have it in my memory. Do you want me to remember the location of "+itemName+" ?", false, {
+                        "itemName": itemName,
+                        "context": "SaveThingsContext"
+                    }, context);
+                }
+
+            } else if(intentContext && intentContext === "OthersMemory") {
+                if (rows[0] !== undefined) {
+                    var mem = rows[0].memory;
+                    respondBack(mem, true, {}, context);
+                } else {
+                    respondBack("Sorry I don't have any memories right now.", false, {}, context);
+                }
+
+            } else {
+                respondBack("Sorry I cannot handle your request right now.", true, {}, context);
+            }
+        });
+    }
+}
+
+
+// Speechlet Helpers
+buildSpeechletResponse = (outputText, shouldEndSession) => {
+
     return {
         outputSpeech: {
             type: "PlainText",
-            text: output
-        },
-        card: {
-            type: "Simple",
-            title: title,
-            content: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
+            text: outputText
         },
         shouldEndSession: shouldEndSession
-    };
+    }
+
 }
 
-function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
-        },
-        shouldEndSession: shouldEndSession
-    };
-}
+generateResponse = (speechletResponse, sessionAttributes) => {
 
-function buildResponse(sessionAttributes, speechletResponse) {
     return {
         version: "1.0",
         sessionAttributes: sessionAttributes,
         response: speechletResponse
-    };
+    }
+
 }
